01d7df2be072dbc9aafd3b38586ee66e7f78b866
marykamau2 <mary.kamau@student.moringaschool.com>
2021-08-26 17:44:32 +0300

    add the requirement.txt file

--- config.py
+++ config.py
@@ -1,39 +1,39 @@
-# import os
+import os
 
-# class Config:
-#     BASE_URL='http://quotes.stormconsultancy.co.uk/random.json'
-#     SECRET_KEY = os.environ.get("SECRET_KEY")
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://marynjeri:njeri2018@localhost/blogs'
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-#     UPLOADED_PHOTOS_DEST ='app/static/photos'
+class Config:
+    BASE_URL='http://quotes.stormconsultancy.co.uk/random.json'
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://marynjeri:njeri2018@localhost/blogs'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
-#     #  email configurations
-#     MAIL_SERVER = 'smtp.googlemail.com'
-#     MAIL_PORT = 587
-#     MAIL_USE_TLS = True
-#     MAIL_USERNAME =os.environ.get("MAIL_USERNAME")
-#     MAIL_PASSWORD =os.environ.get("MAIL_PASSWORD")
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME =os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD =os.environ.get("MAIL_PASSWORD")
     
-#     # simple mde  configurations
-#     SIMPLEMDE_JS_IIFE = True
-#     SIMPLEMDE_USE_CDN = True
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
 
 
-# class ProdConfig(Config):
-#     '''
-#     Production  configuration child class
-#     Args:
-#         Config: The parent configuration class with General configuration settings
-#     '''
-#     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
    
 
 
-# class DevConfig(Config):
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://marynjeri:njeri2018@localhost/blogs'
-#     DEBUG = True
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://marynjeri:njeri2018@localhost/blogs'
+    DEBUG = True
 
-# config_options = {
-# 'development':DevConfig,
-# 'production':ProdConfig,
-# }
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+}
