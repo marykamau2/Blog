b9ab1914a46298c30bc59de89ca839c37550c602
marykamau2 <mary.kamau@student.moringaschool.com>
2021-08-26 10:56:47 +0300

    added class blog

--- app/models.py
+++ app/models.py
@@ -42,44 +42,44 @@
 #         return f'User {self.username}'
         
 
-# class Blog(db.Model):
-#     __tablename__ = 'blogs'
-#     id = db.Column(db.Integer, primary_key=True)
-#     date = db.Column(db.DateTime, default=datetime.utcnow)
-#     title_blog = db.Column(db.String(255), index=True)
-#     description = db.Column(db.String(255), index=True)
-#     user_id = db.Column(db.Integer, db.ForeignKey('users.id',ondelete='CASCADE'), nullable=False)
+class Blog(db.Model):
+    __tablename__ = 'blogs'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    title_blog = db.Column(db.String(255), index=True)
+    description = db.Column(db.String(255), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id',ondelete='CASCADE'), nullable=False)
    
-#     def save_blog(self):
-#         db.session.add(self)
-#         db.session.commit()
-#     @classmethod
-#     def get_blogs(cls, id):
-#         blogs = Blog.query.filter_by(id=id).all()
-#         return blogs
-#     @classmethod
-#     def get_all_blogs(cls):
-#         blogs = Blog.query.order_by('-id').all()
-#         return blogs
-#     def __repr__(self):
-#         return f'Blogs {self.blog_title}'
+    def save_blog(self):
+        db.session.add(self)
+        db.session.commit()
+    @classmethod
+    def get_blogs(cls, id):
+        blogs = Blog.query.filter_by(id=id).all()
+        return blogs
+    @classmethod
+    def get_all_blogs(cls):
+        blogs = Blog.query.order_by('-id').all()
+        return blogs
+    def __repr__(self):
+        return f'Blogs {self.blog_title}'
 
-# class Comment(db.Model):
-#     __tablename__ = 'comments'
-#     id = db.Column(db.Integer, primary_key=True)
-#     comment = db.Column(db.Text())
-#     blog_id = db.Column(db.Integer, db.ForeignKey('blogs.id',ondelete='CASCADE'))
-#     user_id = db.Column(db.Integer, db.ForeignKey('users.id',ondelete='CASCADE'))
-#     date = db.Column(db.DateTime, default=datetime.utcnow)
-#     def save_comment(self):
-#         db.session.add(self)
-#         db.session.commit()
-#     @classmethod
-#     def get_comments(cls, blog_id):
-#         comments = Comment.query.filter_by(blog_id=blog_id).all()
-#         return comments
-#     def delete_comment(self):
-#         db.session.delete(self)
-#         db.session.commit()
-#     def __repr__(self):
-#         return f'Comments: {self.comment}'
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.Text())
+    blog_id = db.Column(db.Integer, db.ForeignKey('blogs.id',ondelete='CASCADE'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id',ondelete='CASCADE'))
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+    @classmethod
+    def get_comments(cls, blog_id):
+        comments = Comment.query.filter_by(blog_id=blog_id).all()
+        return comments
+    def delete_comment(self):
+        db.session.delete(self)
+        db.session.commit()
+    def __repr__(self):
+        return f'Comments: {self.comment}'
